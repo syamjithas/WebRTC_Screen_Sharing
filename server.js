64dbce3dd7e4ee09f2318b649034d0328e3ae3ef
syamjithas <syamjithas@gmail.com>
2021-03-11 13:02:32 +0400

    server

--- server.js
+++ server.js
@@ -2,24 +2,8 @@ const express = require('express')
 const app = express()
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
-const { v4: uuidV4 } = require('uuid')
 
 app.use('/static', express.static('public'))
-
-
-
-// app.get('/', (req, res) => {
-//   res.redirect(`/host/${uuidV4()}`)
-// })
-
-
-// app.get('/host/:room', (req, res) => {
-//   res.render('host', { roomId: req.params.room })
-// })
-
-// app.get('/room/:room', (req, res) => {
-//   res.render('room', { roomId: req.params.room })
-// })
 
 io.on('connection', socket => {
   socket.on('join-room', (roomId, userId) => {
--- startpeer.bat
+++ startpeer.bat
@@ -1,1 +1,7 @@
 peerjs --port 3001 --key peerjs --path /mypeer
+
+npm install -g qckwinsvc
+
+https://www.npmjs.com/package/qckwinsvc
+
+D:\Coding\WebRTC_Screen_Sharing\server.js
